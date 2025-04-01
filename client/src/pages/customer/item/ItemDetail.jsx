953195c771d043a28a044455a436a4d7fa3d6551
Ajibola Sadiq <ajibolasadiq@yahoo.com>
2025-04-01 09:09:41 +0100

    committing new

--- client/src/pages/customer/item/ItemDetail.jsx
+++ client/src/pages/customer/item/ItemDetail.jsx
@@ -6,19 +6,19 @@ const ItemDetail = () => {
     const location = useLocation()
     const { item } = location.state
     return (
-        <div className="w-full  md:flex-row flex flex-col bg-white pt-8 p-5">
-            <div className="md:w-1/2 md:h-full flex justify-center w-full h-[230px]">
-            <div className="!w-[200px] !h-[200px] rounded-full overflow-hidden flex justify-center items-center">
-            <ItemImage style={''} {...{ item }} />
+        <div className="w-full  md:flex-row items-center flex flex-col bg-white pt-8 p-5">
+            <div className="md:w-1/2 md:h-full flex justify-center ">
+                <div className="!w-[200px] !h-[200px] rounded-full overflow-hidden flex justify-center items-center">
+                    <ItemImage style={'h-full w-full'} {...{ item }} />
+                </div>
             </div>
-            </div>
-            <div className="md:w-1/2 w-full">
+            <div className="md:w-1/2 w-full mt-8 md:mt-0">
                 <p className="text-2xl font-semibold">{item.name}</p>
                 <p className="mt-4 text-gray-400 text-sm md:w-2/3 whitespace-normal">{item.description}</p>
                 <p className="mt-4 text-3xl  text-[#fdb750] font-bold">€{item.price}</p>
                 <p className=" mt-3 text-green-600">{item.status == "ONLINE" ? "Available" : "Unavailable"}</p>
                 <div className="w-2/3 h-20 mt-8 flex items-center gap-4">
-                   <AddToCartButton {...{item}}/>
+                    <AddToCartButton {...{ item }} />
                 </div>
             </div>
         </div>
