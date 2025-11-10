ecc6b3e9060234920fdf8da3aaa3b77cfb4b77aa
Ajibola Sadiq <ajibolasadiq@yahoo.com>
2025-11-10 07:16:42 +0100

    working on catering

--- client/src/pages/customer/services/Services.jsx
+++ client/src/pages/customer/services/Services.jsx
@@ -6,6 +6,7 @@ import Catering from './Catering'
 import { Button } from '@/components/button/Button'
 import { motion } from "framer-motion";
 import ScrollFadeIn from '@/components/animations/ScrollFadeIn'
+import { PiChefHatThin } from "react-icons/pi";
 
 
 const Services = () => {
@@ -27,7 +28,7 @@ const Services = () => {
           From intimate gatherings to large events, we ensure quality, flavor, and timely service. Our menu includes native rice, moin moin, jollof pasta, meats, pastries, and more.
           With attention to detail and customer satisfaction at the heart of what we do, we make every event memorable with authentic
           taste and exceptional service.</p>
-        <Button {...{ className: '!rounded-full !mx-auto  md:!w-[200px] py-3 mt-12 md:mt-20 !font-semibold' }}>Contact Us</Button>
+        <Button {...{ className: '!rounded-full !mx-auto  !w-[200px] py-3 mt-12 md:mt-20 !font-semibold' }}>Contact Us</Button>
       </ScrollFadeIn>
       <ScrollFadeIn
         initial={{ opacity: 0, scale: 0.5 }}
@@ -40,7 +41,7 @@ const Services = () => {
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.5 }} className='mt-10 px-3'>
-        <p className='text-5xl font-semibold'>Our Services - Bringing the Taste of Nigeria to the UK</p>
+        <p className='text-5xl font-semibold'>Bringing the Taste of Nigeria to the UK</p>
         <p className='mt-6'>At Dami Cooks, we’re passionate about delivering authentic Nigerian cuisine right to your doorstep. Whether you're craving traditional meals or quick snacks, our app makes it fast,
           easy, and enjoyable to experience the rich flavors of Nigeria — anywhere in the UK.</p>
       </ScrollFadeIn>
@@ -49,30 +50,36 @@ const Services = () => {
         <Pastry />
         <Catering />
       </div>
-       <ScrollFadeIn
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-          className='w-full h-[500px] object-contain overflow-hidden mb-6 md:mb-[500px]'>
-          <Image style={'!w-full'} src={'/images/catering3.jpg'} />
-        </ScrollFadeIn>
-      <div className="md:absolute w-full  gap-x-5 mb-0 flex md:justify-between md:pr-[1px] items-center flex-col md:flex-row h-[840px]
-                  md:h-[400px] bg-[#fdb750] text-red-700 left-0 md:px-10 py-3 md:-bottom-[153rem] ">
-        <div className="md:w-[450px] md:ml-20 ml-20 flex justify-center md:justify-start items-center w-full mx-auto object-cover md:-mt-4 h-[300px] md:h-[100px] ">
-          <img className="flex justify-center mx-auto md:justify-start md:mr-0" src={'/images/FOOD3.png'} />
-        </div>
-        <div className="h-[200px] p-3 md:w-[500px] md:absolute md:left-[500px] md:mr-20  md:mt-0 w-full  flex flex-col items-center">
-          <p className="text-4xl font-extrabold  mb-5">We are ready</p>
-          <p className="text-center">Join thousands of food lovers who trust us to bring a taste of home, every day.</p>
-          <p className="mt-2 text-center">Rediscover the joy of African cuisine. From traditional stews to local street favorites, browse our menu and place your order with ease — we’ll handle the rest.</p>
-          <Button className={'mt-6 !py-3 !rounded-full !w-[200px]'}>Place Order</Button>
-        </div>
-        <div className="md:w-[450px] md:ml-60  mt-20 flex justify-center md:justify-end items-center w-full mx-auto object-cover md:-mt-4 h-[300px] md:h-[100px] ">
-          <img src="/images/DeliveryMan.png" />
-        </div>
-      </div>
+      <ScrollFadeIn
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+        className='w-full h-[500px] object-contain mb-[100px] overflow-hidden md:mb-[50px]'>
+        <Image style={'!w-full'} src={'/images/catering3.jpg'} />
+      </ScrollFadeIn>
+      <ScrollFadeIn
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }} className='mt-10 px-3 mb-32'>
+        <p className='text-5xl font-semibold'>Let's make your event unforgettable</p>
+        <p className='mt-6'>Whether you’re hosting 20 or 200 guests, our team ensures delicious meals, professional service, and a seamless dining experience from start to finish.</p>
+        <p className='font-bold mt-4 text-xl underline'>What We Offer:</p>
+        <ul className='list-none text-lg'>
+          <li className='flex items-center gap-2'><Bullet /> Customized menu options to suit your guests’ taste</li>
+          <li className='flex items-center gap-2'><Bullet /> On-site service or buffet-style setup
+          </li>
+          <li className='flex items-center gap-2'><Bullet /> Pastries, finger foods, and full-course meals</li>
+          <li className='flex items-center gap-2'><Bullet /> Flexible packages to fit your budget
+          </li>
+        </ul>
+        <Button {...{ className: '!rounded-full !mx-auto  !w-[200px] py-3 mt-12 md:mt-20 !font-semibold' }}>Contact Us</Button>
+      </ScrollFadeIn>
     </div>
   )
+}
+
+const Bullet = () => {
+  return <div className='text-red-700 '><PiChefHatThin /></div>
 }
 
 export default Services
