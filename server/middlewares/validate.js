2d2c5895ae5e08163644d02bd5d3b857d2af3c34
Ajibola Sadiq <ajibolasadiq@yahoo.com>
2026-01-13 16:48:21 +0100

    debugging token

--- server/middlewares/validate.js
+++ server/middlewares/validate.js
@@ -1,11 +1,15 @@
 import User from "../models/User.js";
 import jwt from 'jsonwebtoken'
+import dotenv  from 'dotenv'
 
+dotenv.config()
 export const authMiddleware = async (req, res, next) => {
   let token = req.cookies.token;
+  console.log("cookie token", token)
   if(!token){
     const authHeader = req.headers['Authorization'];
     token = authHeader && authHeader.split(' ')[1];
+    console.log("header token", token)
   }
   if (!token) {
     return res.status(401).json({ error: 'Unauthorized' });
