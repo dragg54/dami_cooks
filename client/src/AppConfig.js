a1bbe2a0b56e349e999c17924d7215dc73ed64cf
Ajibola Sadiq <ajibolasadiq@yahoo.com>
2026-02-15 04:26:16 +0100

    pushing to production

--- client/src/AppConfig.js
+++ client/src/AppConfig.js
@@ -1,3 +1,3 @@
-export const BACKEND_SERVER_URL = "http://89.167.40.98:8080"
+export const BACKEND_SERVER_URL = "https://api.damicooks.com"
 export const BACKEND_SERVER_VERSION = 'api/v1'
 // export const BACKEND_SERVER_URL = "https://dami-cooks.onrender.com"
