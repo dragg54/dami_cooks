cce7c3ee7a0bab665bd5d2fb01debc49a24f35b0
Ajibola Sadiq <ajibolasadiq@yahoo.com>
2026-03-22 07:35:24 +0100

    committing new

--- client/src/pages/customer/checkout/Payment.jsx
+++ client/src/pages/customer/checkout/Payment.jsx
@@ -10,7 +10,6 @@ import PaymentForm from "./PaymentForm";
 import { Button } from "../../../components/button/Button";
 
 const Payment = ({deliveryDetails, clientSecret, setClientSecret, shippingChargeResponse}) => {
-  console.log("stripe apiiii", import.meta.env.VITE_STRIPE_PUBLIC_KEY)
   const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
   const [ isValidForm, setIsValidForm ] = useState()
   const CARD_OPTIONS = {
